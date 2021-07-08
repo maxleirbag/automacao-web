190040ff21715e9a5d2f5e144272772188be595e
Max <78800453+maxleirbag@users.noreply.github.com>
2021-07-08 12:24:51 -0300

    Link via input no terminal
    Antes era necessário editar o código para alterar o link de interesse, agora no próprio terminal é possível baixar o vídeo

--- youtube-dl.py
+++ youtube-dl.py
@@ -1,13 +1,8 @@
-#import youtube_dl
-#youtube-dl -f ("(bestvideo+bestaudio/best)[protocol^=https://www.youtube.com/watch?v=2UM8Z7zckS4]")
-
 from __future__ import unicode_literals
 import youtube_dl
 
+link = input("Insira o link do vídeo do Youtube que deseja baixar: ")
+
 ydl_opts = {}
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://youtu.be/4z9ADmZhqko'])
-
-
-
-# https://www.youtube.com/watch?v=Q3InFEwJVr0 2.5
+    ydl.download([link])
